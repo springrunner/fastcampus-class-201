c694077847e0f98892d3e8f65f599807bfe3e2fd
Honeymon <ihoneymon@gmail.com>
2022-06-25 20:10:07 +0900

    스프링 부트 운영지원 기능 추가

--- dive-log/src/main/java/com/fastcampus/springrunner/divelog/core/diveresort/application/DiveResortManager.java
+++ dive-log/src/main/java/com/fastcampus/springrunner/divelog/core/diveresort/application/DiveResortManager.java
@@ -33,8 +33,8 @@ public class DiveResortManager implements DiveResortEditor, DiveResortFinder {
     @Transactional(readOnly = true)
     @Override
     public Optional<DiveResortDto> findByDiveResortId(Long diveResortId) {
-        throw new DiveResortNotFoundException(diveResortId);
-//        return repository.findById(diveResortId).map(DiveResortDto::ofEntity);
+//        throw new DiveResortNotFoundException(diveResortId);
+        return repository.findById(diveResortId).map(DiveResortDto::ofEntity);
     }
 
     @Transactional
