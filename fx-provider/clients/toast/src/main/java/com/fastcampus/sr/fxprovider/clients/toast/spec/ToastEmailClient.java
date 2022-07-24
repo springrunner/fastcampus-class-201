aef104b2d95e391da553d29c7a48a29281ff7d8e
Honeymon <ihoneymon@gmail.com>
2022-07-25 00:22:42 +0900

    Toast email 가이드 링크 추가

--- fx-provider/clients/toast/src/main/java/com/fastcampus/sr/fxprovider/clients/toast/spec/ToastEmailClient.java
+++ fx-provider/clients/toast/src/main/java/com/fastcampus/sr/fxprovider/clients/toast/spec/ToastEmailClient.java
@@ -1,5 +1,8 @@
 package com.fastcampus.sr.fxprovider.clients.toast.spec;
 
+/**
+ * @see <a href="https://docs.toast.com/ko/Notification/Email/ko/api-guide/">Notification > Email > API v2.0 가이드</a>
+ */
 public interface ToastEmailClient {
     ToastEmailResponse send(ToastEmailRequest request);
 }
